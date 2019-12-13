97fe68b7d5f1b526bc8f3d73a52b57a4791154c4
Konrad <konrad.guzior@gmail.com>
2019-12-13 16:38:42 +0100

    Tutorial moved to a separate file.

--- main.js
+++ main.js
@@ -55,28 +55,8 @@ function Main(turnpoint, airport, startLine, thermalArray, background){
 				menuButton.update();
 			}
 			if (tutorial) {
-				for (var i = 0; i < tutorialItem.length; i++) {
-					if(i == 0 || i == 1 || i == 4 && !cloudBase && !turnpoint.tp){
-						tutorialItem[i].vis = true;
-					}
-					if (i == 1 | i == 0 && cloudBase && !turnpoint.tp) {
-						tutorialItem[i].vis = false;
-					}
-					if (i == 2 | i == 3 && cloudBase && !turnpoint.tp) {
-						tutorialItem[i].vis = true;
-					}
-					if (i == 0 | i == 1 | i == 2 | i == 3 && turnpoint.tp) {
-						tutorialItem[i].vis = false;
-					}
-					if (i == 5 | i == 6 | i == 7 && turnpoint.tp) {
-						tutorialItem[i].vis = true;
-					}
-					tutorialItem[i].update();
-				}
-				for (var i = 0; i < tutorialStar.length; i++) {
-					tutorialStar[i].update();
-				}
-		}
+				mainTutorial();
+			}
 		startScore();
 	})();
 
